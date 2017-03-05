4ed803acb18763408c72ddff000b2e75b6aac655
Alex Gyori <gyori@illinois.edu>
2017-03-05 01:07:28 +0000

    stuff

--- src/test/java/edu/illinois/testing/AppTest.java
+++ src/test/java/edu/illinois/testing/AppTest.java
@@ -1,38 +1,22 @@
 package edu.illinois.testing;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
-        assertTrue( true );
+
+	String value = System.getenv("MYVARIABLE");
+	
+        assertEquals("2w2344", value);
     }
 }
