be48fd3a939b6489a3e4c962b2364b5ea65be2f5
Alex Gyori <gyori@illinois.edu>
2017-03-05 00:59:36 +0000

    dummy project

---
+++ src/main/java/edu/illinois/testing/App.java
@@ -0,0 +1,13 @@
+package edu.illinois.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
